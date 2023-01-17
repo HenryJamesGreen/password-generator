f4315bdf8d8488059696da7d89c3af69db6fbe40
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-01-17 13:17:50 +0000

    added password options, using confirm boxes and separate statements, rather than forEach (as last time)

--- script.js
+++ script.js
@@ -1,118 +1,147 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
+
+let userOptions = [
+  specialCharacters,
+  upperCasedCharacters,
+  lowerCasedCharacters,
+  numericCharacters,
+];
+
+let passwordLength = [];
+let passwordchoices = [];
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let passwordLength = parseInt(prompt("Please specify password length 10-64"));
 
+  if (passwordLength >= 10 && passwordLength <= 64) {
+    let userTellSpec = confirm("Would you like to use special characters?");
+    if (userTellSpec === true) {
+      passwordchoices.push(specialCharacters);
+    }
+    let userTellUp = confirm("Would you like to use upper case characters?");
+    if (userTellUp === true) {
+      passwordchoices.push(upperCasedCharacters);
+    }
+    let userTellDo = confirm("Would you like to use lower case characters?");
+    if (userTellDo === true) {
+      passwordchoices.push(lowerCasedCharacters);
+    }
+    let userTellNum = confirm("Would you like to use numeric characters?");
+    if (userTellNum === true) {
+      passwordchoices.push(numericCharacters);
+    }
+  }
+  return userOptions;
 }
+
+getPasswordOptions();
+console.log(passwordchoices);
 
 // Function for getting a random element from an array
-function getRandom(arr) {
-
-}
+function getRandom(arr) {}
 
 // Function to generate password with user input
-function generatePassword() {
-
-}
+function generatePassword() {}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
